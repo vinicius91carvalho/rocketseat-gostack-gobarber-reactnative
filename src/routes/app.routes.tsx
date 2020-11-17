2f78114bf2e62e7f66acd99f9dd40ba3893c7f7f
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2020-11-17 10:09:35 -0300

    Create others routes of app

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
@@ -13,6 +16,12 @@ const AppRoutes: React.FC = () => (
         }}
     >
         <App.Screen name="Dashboard" component={Dashboard} />
+
+        {/* Rotas de agendamento */}
+        <App.Screen name="CreateAppointment" component={CreateAppointment} />
+        <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+        <App.Screen name="Profile" component={Profile} />
     </App.Navigator>
 );
 
